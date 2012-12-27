adad02221a7eebe7afeb067593e6edf9194d8834
Subin Sebastian <subinsebastien@gmail.com>
2012-12-28 02:07:10 +0530

    The initial working commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ console.log("About to start server");
 var http = require("http");
 var port = process.env.PORT || 8888;
 
+//The start method
 function start(route, handle)	{
 	var server = http.createServer(function(request, response)	{
 		route(handle, request, response);
