2f562fdf20a74e5aa2034fb31aac72e8d8eb3dbf
Subin Sebastian <subinsebastien@gmail.com>
2012-12-28 02:04:57 +0530

    Retry 2

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 console.log("About to start server");
 
 var http = require("http");
+var port = process.env.PORT || 8888;
 
 function start(route, handle)	{
 	var server = http.createServer(function(request, response)	{
 		route(handle, request, response);
 	});
-	server.listen(80);
-	console.log("Listening on port 8888");
+	server.listen(port);
+	console.log("Server Running...");
 }
 
 exports.start = start;
