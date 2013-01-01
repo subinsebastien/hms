5eb0003595ae3de25a62b5139fd17a480573de33
Bijimon AP <bijimon@top3dentists.com>
2013-01-01 17:57:03 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>HMS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 30px;
+        padding-bottom: 30px;
+      }
+    </style>
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    
+  </head>
+
+  <body>
+<div class="container">
+<div class="row-fluid">
+<div class="span10">
+<div class="hrclass"></div>
+<div id="timeline" class="row-fluid">
+	<div class="span2 offset1"><span class="badge">1</span></div>
+	<div class="span2 offset1"><span class="badge">2</span></div>
+	<div class="span2 offset1"><span class="badge">3</span></div>
+	<div class="span2 offset1"><span class="badge">4</span></div>
+</div></div></div><br>
+	    <div class="row-fluid">
+		    <div class="span6">Fluid 7</div> 
+		    <div class="span2 offset2">
+		    <form class="container">
+
+	      <div class="row">
+	        <div class="span4">
+
+	          <input type="text" value="" placeholder="Hazard tittle">
+	
+	          <br><br>
+	
+	          <input type="text" value="" placeholder="Your Name">
+	
+	          <br><br>
+	
+	          <input type="email" value="" placeholder="Your Email Address">
+	
+	          <br><br>
+	          
+	          <input type="file" value="">
+	          
+	          <br><br>
+			<p><button class="btn btn-primary" type="button">Next</button></p>
+	          
+	        </div><!-- /span4 -->
+	      </div><!-- /row -->
+
+    </form></div>
+ </div>
+ </div>
+  </body>
+</html>
