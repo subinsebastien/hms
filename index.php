df1b36aedaf44078ac30b0455101190ba3f84422
Bijimon AP <bijimon@top3dentists.com>
2013-01-01 18:30:15 +0530

    second commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <title>HMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,49 +24,57 @@
 
     <!-- Le fav and touch icons -->
     
-  </head>
+</head>
 
-  <body>
-<div class="container">
-<div class="row-fluid">
-<div class="span10">
-<div class="hrclass"></div>
-<div id="timeline" class="row-fluid">
-	<div class="span2 offset1"><span class="badge">1</span></div>
-	<div class="span2 offset1"><span class="badge">2</span></div>
-	<div class="span2 offset1"><span class="badge">3</span></div>
-	<div class="span2 offset1"><span class="badge">4</span></div>
-</div></div></div><br>
-	    <div class="row-fluid">
-		    <div class="span6">Fluid 7</div> 
-		    <div class="span2 offset2">
-		    <form class="container">
-
-	      <div class="row">
-	        <div class="span4">
-
-	          <input type="text" value="" placeholder="Hazard tittle">
-	
-	          <br><br>
-	
-	          <input type="text" value="" placeholder="Your Name">
-	
-	          <br><br>
-	
-	          <input type="email" value="" placeholder="Your Email Address">
-	
-	          <br><br>
-	          
-	          <input type="file" value="">
-	          
-	          <br><br>
-			<p><button class="btn btn-primary" type="button">Next</button></p>
-	          
-	        </div><!-- /span4 -->
-	      </div><!-- /row -->
-
-    </form></div>
- </div>
- </div>
-  </body>
+<body>
+  <div class="tittle"><h1>Hazard Management System</h1></div>
+		<div class="container">
+		
+			<div class="row-fluid">
+			<div class="span10">
+				
+				<div class="hrclass"></div>
+				<div id="timeline" class="row-fluid">
+					<div class="span2 offset1"><span class="badge">1</span></div>
+					<div class="span2 offset1"><span class="badge">2</span></div>
+					<div class="span2 offset1"><span class="badge">3</span></div>
+					<div class="span2 offset1"><span class="badge">4</span></div>
+				</div>
+			
+			</div> <!--end span 10-->
+			</div> <!--end row-fluid--><br>
+				    
+			<div class="row-fluid">
+				<div class="span6">Fluid 7</div> 
+					    
+				<div class="span2 offset2">
+					<form class="container">
+				      	<div class="row">
+				        <div class="span4">
+			
+				        <input type="text" value="" placeholder="Hazard tittle">
+				
+			          <br><br>
+			
+			          	<input type="text" value="" placeholder="Your Name">
+			
+			          <br><br>
+			
+			          	<input type="email" value="" placeholder="Your Email Address">
+			
+			          <br><br>
+			          
+			          	<input type="file" value="">
+			          
+			          <br><br>
+						<p><button class="btn btn-primary" type="button">Next</button></p>
+			          
+			        	</div><!-- end span4 -->
+			      		</div><!-- end row -->
+		    		</form>
+		    	</div> <!--end span 2-->
+			</div> <!--end row-fuid-->
+		</div> <!--end container-->						
+  
+</body>
 </html>
