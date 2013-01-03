9af1501d777db63473071b0591c7ffeb9805c41e
Bijimon AP <bijimon@top3dentists.com>
2013-01-03 16:35:00 +0530

    4th commit

--- index.php
+++ index.php
@@ -30,51 +30,44 @@
   <div class="tittle"><h1>Hazard Management System</h1></div>
 		<div class="container">
 		
-			<div class="row-fluid">
+			<div class="row-fluid" style="margin-bottom:45px;">
 			<div class="span12">
 				
 				<div class="hrclass"></div>
 				<div id="timeline" class="row-fluid">
-					<div class="span2 offset1"><span class="badge">1</span></div>
-					<div class="span2 offset1"><span class="badge">2</span></div>
-					<div class="span2 offset1"><span class="badge">3</span></div>
-					<div class="span2 offset1"><span class="badge">4</span></div>
+					<div class="span2 offset1"><span class="badge">1</span><div class="badgetittle1">Add Hazard Details</div></div>
+					<div class="span2 offset1"><span class="badge">2</span><div class="badgetittle2">Description and details</div></div>
+					<div class="span2 offset1"><span class="badge">3</span><div class="badgetittle3">Share on Social Networks</div></div>
+					<div class="span2 offset1"><span class="badge">4</span><div class="badgetittle4">Complete!</div></div>
 				</div>
 			
-			</div> <!--end span 10-->
-			</div> <!--end row-fluid--><br>
+			</div> <!--end span 12-->
+			</div> <!--end row-fluid-->
 				    
 			<div class="row-fluid">
-				<div class="span6">Fluid 7</div> 
-					    
-				<div class="span2 offset2">
-					<form class="container">
-				      	<div class="row">
-				        <div class="span4">
-			
-				        <input type="text" value="" placeholder="Hazard tittle">
+				<div class="span6">
 				
-			          <br><br>
-			
-			          	<input type="text" value="" placeholder="Your Name">
-			
-			          <br><br>
-			
-			          	<input type="email" value="" placeholder="Your Email Address">
-			
-			          <br><br>
-			          
-			          	<input type="file" value="">
-			          
-			          <br><br>
-						<p><button class="btn btn-primary" type="button">Next</button></p>
-			          
-			        	</div><!-- end span4 -->
-			      		</div><!-- end row -->
+				<iframe width="568" height="467" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=200859496238237925369.0004d25f25ed9b9539bb3&amp;ie=UTF8&amp;t=m&amp;ll=10.019694,76.31773&amp;spn=0.020539,0.046348&amp;output=embed"></iframe><br /><small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=200859496238237925369.0004d25f25ed9b9539bb3&amp;ie=UTF8&amp;t=m&amp;ll=10.019694,76.31773&amp;spn=0.020539,0.046348&amp;source=embed" style="color:#0000FF;text-align:left">HMS</a> in a larger map</small>
+				
+				</div> 
+					    
+				<div class="span6">
+					<form>
+					
+				      <input type="text" value="" placeholder="Hazard tittle">
+			          <input type="text" value="" placeholder="Your Name">
+			          <input type="email" value="" placeholder="Your Email Address">
+			          <input type="file" value="">
+			          <label class="checkbox">
+						<input type="checkbox"> I accept the T&C
+					   </label>
+					   <p><button class="btn btn-primary" type="button">Next</button></p>
+
 		    		</form>
 		    	</div> <!--end span 2-->
-			</div> <!--end row-fuid-->
-		</div> <!--end container-->						
+		    	
+		</div> <!--end row-fuid-->
+	</div> <!--end container-->						
   
 </body>
 </html>
