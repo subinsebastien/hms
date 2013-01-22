efe47afa2d83055f0656f3f5013c5bf4cc3b1d54
Bijimon AP <bijimon@top3dentists.com>
2013-01-22 16:05:20 +0530

    commit

---
+++ list.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+
+<title>HMS</title>
+
+</head>
+
+<body>
+
+<?php
+
+$ch = curl_init();
+
+// Setting curl options
+
+curl_setopt($ch, CURLOPT_URL, "https://api.parse.com/1/functions/list");
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//Setting Parse API keys and IDs
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+
+'X-Parse-Application-Id: uDAS1lt0f2SyOyL4XrgEtHBqZdxRlUpNVwlVBYdY',
+
+'X-Parse-REST-API-Key: aVL9vXIQ2E5iOCsFfvgtMHj0Ldlr03aDdY3XcR24',
+
+'Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{}');
+
+// Finally let it run
+
+$data = curl_exec($ch);
+print_r($data);exit;
+curl_close($ch);
+
+return $data;
+
+?>
+
+</body>
+</head>
+</html>
