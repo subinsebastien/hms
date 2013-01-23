70e5bc62dc58e25b73600fa9bf4b8b0613ca20f5
Bijimon AP <bijimon@top3dentists.com>
2013-01-23 09:58:11 +0530

    commit

--- new.php
+++ new.php
@@ -12,48 +12,6 @@
 
 <body>
 
-<!-- Define a form to fill up and set correct parameter names -->
-
-<form action="new.php" method="POST">
-
-<label for="title">Title:</label>
-
-<br>
-
-<input type="text" name="title" value="<?php echo @$_POST['title']; ?>" placeholder="Title" ><br/>
-
-<br>
-
-<label for="name">Name:</label>
-
-<br>
-
-<input type="text" name="reportedBy" value="<?php echo @$_POST['name'];?>" placeholder="Name" ><br/>
-
-<br>
-
-<label for="email">Email:</label>
-
-<br>
-
-<input type="email" placeholder="Email" name="reportedByEmail" value="<?php echo @$_POST['email'];?>" ><br/>
-
-<br>
-
-<label for="description">Description:</label>
-
-<br>
-
-<textarea name="description" placeholder="Description" value="<?php echo @$_POST['description'];?>"></textarea>
-
-<br>
-
-<br>
-
-<input type="submit" value="Submit">
-
-</form>
-
 <!-- PHP stuff starts here, bingo! -->
 
 <?php
