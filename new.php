832d9bc89e55837b148164fa44278fba0133a3e1
Stultus <hrishi.kb@gmail.com>
2013-01-25 10:55:29 +0530

    upload half work done

--- new.php
+++ new.php
@@ -1,24 +1,52 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-
-<meta charset="utf-8">
-
-<title>HMS</title>
-
-</head>
-
-<body>
-
-<!-- PHP stuff starts here, bingo! -->
-
 <?php
 
 if (isset($_POST)) {
 
+$jason=array();
 $data = json_encode($_POST);
+
+
+$target_path = "uploads/";
+$img =rand().basename( $_FILES['image']['name']);
+$target_path=$target_path.$img;
+
+
+if(move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api.parse.com/1/files/".$img);
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//Setting Parse API keys and IDs as POST headers
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+
+'X-Parse-Application-Id: uDAS1lt0f2SyOyL4XrgEtHBqZdxRlUpNVwlVBYdY',
+
+'X-Parse-REST-API-Key: aVL9vXIQ2E5iOCsFfvgtMHj0Ldlr03aDdY3XcR24',
+
+'Content-Type: application/json'));
+
+$response = curl_exec($ch);
+
+print($response);
+print($img);
+
+} else{
+    echo "There was an error uploading the file, please try again!";
+}
+
+$json = json_decode($data,true);
+$json['imageUrl']='test';
+$data = json_encode($json);
+//print($response);
+
+
 
 $ch = curl_init();
 
@@ -44,16 +72,11 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
 // Finally let it run
 
-$response = curl_exec($ch);
+//$response = curl_exec($ch);
 
 curl_close($ch);
 
 }
 
-header('URL=list.php');
-
+//header('Refresh:0;URL=list.php');
 ?>
-
-</body>
-
-</html>
