3b1443cabef3b48b79051f17d8fcf52500649db4
Napster <napster@panther.(none)>
2013-01-31 18:40:24 +0530

    Another test

--- vid_count.php
+++ vid_count.php
@@ -3,5 +3,15 @@ $video_ID = 'RveIJixVs4M';
 $JSON = file_get_contents("https://gdata.youtube.com/feeds/api/videos/{$video_ID}?v=2&alt=json");
 $JSON_Data = json_decode($JSON);
 $views = $JSON_Data->{'entry'}->{'yt$statistics'}->{'viewCount'};
+
+//mail
+$deepak = "deepak@emdesic.com"
+$subject = "Kumaran and Pornstar video has reached " . $views . " hits";
+$headers = "From: kumar@youtube.com" . "\r\n" . "CC: subinsebastien@gmail.com";
+$text = " ";
+
+mail($deepak, $subject, $text, $headers);
+
+
 echo $views;
 ?>
