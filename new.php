2adc09232daadf0020bc721f71de26d3b65e20e8
Stultus <hrishi.kb@gmail.com>
2013-02-05 23:24:07 +0530

    send the temp file itself

--- new.php
+++ new.php
@@ -7,13 +7,13 @@ if (isset($_POST)) {
 		$data = json_encode($_POST);
 		$appid ='uDAS1lt0f2SyOyL4XrgEtHBqZdxRlUpNVwlVBYdY';
 		$api_key='aVL9vXIQ2E5iOCsFfvgtMHj0Ldlr03aDdY3XcR24';
-		$target_path = "/var/cache/";
+		$target_path = "uploads/";
 		
 		$img =rand().basename( $_FILES['image']['name']);
 		$target_path = $target_path.$img;
 		
 
-		if(move_uploaded_file($_FILES['image']['tmp_name'],$target_path)) {
+		//if(move_uploaded_file($_FILES['image']['tmp_name'],$target_path)) {
 			//send the image to server
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, "https://api.parse.com/1/files/".$img);
@@ -24,7 +24,8 @@ if (isset($_POST)) {
 								    'X-Parse-Application-Id:'.$appid,
 								    'X-Parse-REST-API-Key:'.$api_key,
 								    'Content-Type: image/jpeg'));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents($target_path));
+			//curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents($target_path));
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents($_FILES['image']['tmp_name']));
             $response = curl_exec($ch);
             //get the image url 
 			$url = json_decode($response,true);
@@ -44,9 +45,9 @@ if (isset($_POST)) {
 			$response = curl_exec($ch);
 			curl_close($ch);
 
-		} else{
-		        echo "There was an error uploading the file, please try again!";
-		}
+//		} else{
+//		        echo "There was an error uploading the file, please try again!";
+//		}
 
 	}
 header('Refresh:0;URL=list.php');	
